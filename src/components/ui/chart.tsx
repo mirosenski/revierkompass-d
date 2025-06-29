f14c41fdaa95decf17796c67503a99e1ef89921b
Igor Mirosenski <igor.mirosenski@polizei-bw.de>
2025-06-29 08:42:41 +0200

    React Key-Warnungen behoben - Chart-Komponente korrigiert

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -192,7 +192,7 @@ const ChartTooltipContent = React.forwardRef<
 
             return (
               <div
-                key={item.dataKey}
+                key={`${item.dataKey}-${index}`}
                 className={cn(
                   "flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-zinc-500 dark:[&>svg]:text-zinc-400",
                   indicator === "dot" && "items-center"
@@ -291,7 +291,7 @@ const ChartLegendContent = React.forwardRef<
 
           return (
             <div
-              key={item.value}
+              key={`${item.value}-${item.dataKey}`}
               className={cn(
                 "flex items-center gap-1.5 [&>svg]:h-3 [&>svg]:w-3 [&>svg]:text-zinc-500 dark:[&>svg]:text-zinc-400"
               )}
